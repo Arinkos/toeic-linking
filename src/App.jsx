4e7158903816ad15b1d163b0971e39a1ffc234bb
Arinkos <37305843+Arinkos@users.noreply.github.com>
2026-06-28 13:37:40 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+const SUPA_URL = "https://ruenjdczklrczrqojivq.supabase.co";
+const SUPA_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1ZW5qZGN6a2xyY3pycW9qaXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1ODY4MTcsImV4cCI6MjA5ODE2MjgxN30.05x0XcANGxa-oJf-GPvzCCyiyl58zT98-JfZV5kacms";
+
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
